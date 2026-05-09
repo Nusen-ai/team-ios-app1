6270962e6d7db44e7468917fd709e6db47c5de1c
Nusen1123 <1123904525@qq.com>
2026-05-09 18:07:27 +0800

    feat(FlutterEngineManager): 添加关闭Flutter页面功能
    添加关闭Flutter页面的方法及相关处理逻辑，包括：
    1. 新增closeChannel用于处理关闭页面请求
    2. 添加currentFlutterViewController属性跟踪当前页面
    3. 实现closeFlutterPage方法关闭页面并发送通知
    4. 新增相关通知类型用于页面关闭和导航事件

--- YourApp/Sources/Flutter/FlutterEngineManager.swift
+++ YourApp/Sources/Flutter/FlutterEngineManager.swift
@@ -6,9 +6,11 @@ class FlutterEngineManager {
     static let shared = FlutterEngineManager()
 
     private(set) var engine: FlutterEngine?
+    private(set) var currentFlutterViewController: FlutterViewController?
 
     private var methodChannel: FlutterMethodChannel?
     private var eventChannel: FlutterEventChannel?
+    private var closeChannel: FlutterMethodChannel?
 
     struct Routes {
         static let login = "/login"
@@ -70,6 +72,15 @@ class FlutterEngineManager {
 
         methodChannel?.setMethodCallHandler { [weak self] call, result in
             self?.handleMethodCall(call: call, result: result)
+        }
+
+        closeChannel = FlutterMethodChannel(
+            name: "com.shared.components/close_flutter",
+            binaryMessenger: engine.binaryMessenger
+        )
+
+        closeChannel?.setMethodCallHandler { [weak self] call, result in
+            self?.handleCloseMethodCall(call: call, result: result)
         }
     }
 
@@ -182,12 +193,45 @@ class FlutterEngineManager {
         from viewController: UIViewController
     ) {
         let flutterViewController = initializeEngine()
+        currentFlutterViewController = flutterViewController
 
         if let args = arguments {
             sendMessageToFlutter(method: "setRouteArguments", arguments: args)
         }
 
+        flutterViewController.modalPresentationStyle = .fullScreen
         viewController.present(flutterViewController, animated: true)
+    }
+
+    /// 关闭当前 Flutter 页面，返回到 iOS 原生页面
+    func closeFlutterPage() {
+        if let flutterVC = currentFlutterViewController {
+            flutterVC.dismiss(animated: true) { [weak self] in
+                self?.currentFlutterViewController = nil
+                NotificationCenter.default.post(name: .flutterPageClosed, object: nil)
+            }
+        }
+    }
+
+    /// 处理关闭页面的 MethodCall
+    private func handleCloseMethodCall(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "closeFlutterPage":
+            closeFlutterPage()
+            result(true)
+        case "onPageNavigated":
+            if let args = call.arguments as? [String: Any],
+               let page = args["page"] as? String {
+                NotificationCenter.default.post(
+                    name: .flutterPageNavigated,
+                    object: nil,
+                    userInfo: ["page": page]
+                )
+            }
+            result(true)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
     }
 
     func handleDeepLink(url: URL) {
@@ -223,6 +267,8 @@ extension Notification.Name {
     static let flutterError = Notification.Name("FlutterError")
     static let flutterSurveySubmitted = Notification.Name("FlutterSurveySubmitted")
     static let flutterDeviceInfoReceived = Notification.Name("FlutterDeviceInfoReceived")
+    static let flutterPageClosed = Notification.Name("FlutterPageClosed")
+    static let flutterPageNavigated = Notification.Name("FlutterPageNavigated")
     static let shouldNavigateToLogin = Notification.Name("ShouldNavigateToLogin")
     static let shouldNavigateToShop = Notification.Name("ShouldNavigateToShop")
     static let shouldNavigateToSurvey = Notification.Name("ShouldNavigateToSurvey")
